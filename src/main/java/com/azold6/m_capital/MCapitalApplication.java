dbe36eb51db1933d7c7dd83c59db7870636d3600
Alex <alex.rodrigues23997@gmail.com>
2022-04-27 00:56:48 -0300

    Initial commit

---
+++ src/main/java/com/azold6/m_capital/MCapitalApplication.java
@@ -0,0 +1,13 @@
+package com.azold6.m_capital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCapitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MCapitalApplication.class, args);
+	}
+
+}
