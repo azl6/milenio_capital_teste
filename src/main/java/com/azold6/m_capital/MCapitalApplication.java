c09392325855e9c1fdecd26e96d502280981b397
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-04-29 13:32:40 -0300

    Implementando o algorítmo - P1

--- src/main/java/com/azold6/m_capital/MCapitalApplication.java
+++ src/main/java/com/azold6/m_capital/MCapitalApplication.java
@@ -1,13 +1,54 @@
 package com.azold6.m_capital;
 
+import com.azold6.m_capital.domain.Graph;
+import com.azold6.m_capital.domain.Node;
+import com.azold6.m_capital.services.GraphService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MCapitalApplication {
+public class MCapitalApplication implements CommandLineRunner {
+
+	@Autowired
+	private GraphService graphService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MCapitalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Node nodeA = new Node("A");
+		Node nodeB = new Node("B");
+		Node nodeC = new Node("C");
+		Node nodeD = new Node("D");
+		Node nodeE = new Node("E");
+		Node nodeF = new Node("F");
+
+		nodeA.addDestination(nodeB, 10);
+		nodeA.addDestination(nodeC, 15);
+
+		nodeB.addDestination(nodeD, 12);
+		nodeB.addDestination(nodeF, 15);
+
+		nodeC.addDestination(nodeE, 10);
+
+		nodeD.addDestination(nodeE, 2);
+		nodeD.addDestination(nodeF, 1);
+
+		nodeF.addDestination(nodeE, 5);
+
+		Graph graph = new Graph();
+
+		graph.addNode(nodeA);
+		graph.addNode(nodeB);
+		graph.addNode(nodeC);
+		graph.addNode(nodeD);
+		graph.addNode(nodeE);
+		graph.addNode(nodeF);
+
+		graph = graphService.calculateShortestPathFromSource(graph, nodeA);
+	}
 }
